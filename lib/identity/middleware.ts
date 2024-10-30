eae8b40d8669fbdce3e78b07d0eb57020116b74c
salim laimeche <slaimeche@webexpr.fr>
2024-10-30 12:07:19 +0100

    Improve Login component styles and enhance middleware route handling for consistency

--- lib/identity/middleware.ts
+++ lib/identity/middleware.ts
@@ -1,10 +1,10 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { cookies } from 'next/headers'
-import { decrypt } from './session-local'
+import { NextRequest, NextResponse } from "next/server"
+import { cookies } from "next/headers"
+import { decrypt } from "./session-local"
 
 // 1. Specify protected and public routes
-const protectedRoutes = ['/board','/historique','/profile']
-const publicRoutes = ['/login', '/signup', '/']
+const protectedRoutes = ["/board", "/historique", "/profile"]
+const publicRoutes = ["/login", "/signup", "/", "/video-inference"]
 
 export default async function middleware(req: NextRequest) {
   // 2. Check if the current route is protected or public
@@ -13,20 +13,20 @@ export default async function middleware(req: NextRequest) {
   const isPublicRoute = publicRoutes.includes(path)
 
   // 3. Decrypt the session from the cookie
-  const cookie = cookies().get('session')?.value
+  const cookie = cookies().get("session")?.value
   const session = await decrypt(cookie)
 
   // 4. Redirect
   if (isProtectedRoute && !session?.userId) {
-    return NextResponse.redirect(new URL('/login', req.nextUrl))
+    return NextResponse.redirect(new URL("/login", req.nextUrl))
   }
 
   if (
     isPublicRoute &&
     session?.userId &&
-    !req.nextUrl.pathname.startsWith('/board')
+    !req.nextUrl.pathname.startsWith("/board")
   ) {
-    return NextResponse.redirect(new URL('/board', req.nextUrl))
+    return NextResponse.redirect(new URL("/board", req.nextUrl))
   }
 
   return NextResponse.next()
